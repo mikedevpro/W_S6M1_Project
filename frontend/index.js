52029561e27298d65d43aadba2139cb78cbb12b6
Michael Nobles <Mnobles33@gmail.com>
2024-09-08 06:16:08 -0700

    Final commit

--- frontend/index.js
+++ frontend/index.js
@@ -14,11 +14,11 @@ const root = createRoot(domNode)
 
 root.render(
   <>
-    {/*<Counter /> */}
-    {/*<Moods /> */}
-    {/*<Spinner /> */} 
-    {/*<Input /> */}
+    <Counter /> 
+    <Moods /> 
+    <Spinner />  
+    <Input /> 
     <Squares /> 
-    {/*<Programmers /> */}
+    <Programmers />
   </>
 )
